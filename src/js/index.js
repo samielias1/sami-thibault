8014d4c5148eafc9a002a3974a7e93f963f2ab3c
samielias1 <105636222+samielias1@users.noreply.github.com>
2022-05-30 09:09:43 +0200

    Updates Tibo

--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,41 @@ import 'ol/ol.css';
 import css from '../css/app.scss';
 
 // import {OlMap} from './ol-map';
-import {DummyClass} from './dummy-class';
-import {OlMap} from './ol-map_geojsonFile';
+import { DummyClass } from './dummy-class';
+import { OlMap } from './ol-map_geojsonFile';
+import {arrBezirke, arrUhr, arrJahr} from './list_filters';
 
 
 // alert();
 
 new OlMap();
+
+//Radio button mit Bezirken : im Moment sind Bezirken durch random erstellt und daher falsch!
+var txtBezirk = ['<p>Bezirk</p>'];
+for (let i = 0; i < arrBezirke.length; i++) {
+    let txt1 = '<input type="radio" id="' + arrBezirke[i] + '" name="txtbezirk" value="' + i + '"><br>';
+    let txt2 = '<for"' + arrBezirke[i] + '">' + arrBezirke[i] + '</label><br>';
+    txtBezirk += txt1 + txt2;
+};
+$('#divBezirk').html(txtBezirk);
+
+//Radio button mit Uhrzeiten
+var txtUhr = ['<p>Uhrzeiten</p>'];
+for (let i = 0; i < arrUhr.length; i++) {
+    let txt3 = '<input type="radio" id="' + arrUhr[i] + '" name="txtUhr" value="' + i + '"><br>';
+    let txt4 = '<for"' + arrUhr[i] + '">' + arrUhr[i] + '</label><br>';
+    txtUhr += txt3 + txt4;
+};
+$('#divUhrzeit').html(txtUhr);
+
+//Radio button mit Jahren
+var txtJahr = ['<p>Jahresgang</p>'];
+for (let i = 0; i < arrJahr.length; i++) {
+    let txt5 = '<input type="radio" id="' + arrJahr[i] + '" name="txtJahr" value="' + arrJahr[i] + '"><br>';
+    let txt6 = '<for"' + arrJahr[i] + '">' + arrJahr[i] + '</label><br>';
+    txtJahr += txt5 + txt6;
+};
+$('#divJahr').html(txtJahr);
 
 
 const dummyElement = new DummyClass();
---
+++ src/js/list_filters.js
@@ -0,0 +1,58 @@
+//Liste fuer die Bezirke
+export const arrBezirke = [
+    "Berlin allg.",
+    "Charlottenburg",
+    "Friedrichshain",
+    "Hellersdorf",
+    "Hohenschönhausen",
+    "Köpenick",
+    "Kreuzberg",
+    "Lichtenberg",
+    "Marzahn",
+    "Mitte",
+    "Neukölln",
+    "Pankow",
+    "Prenzlauer Berg",
+    "Reinickendorf",
+    "Schöneberg",
+    "Tempelhof",
+    "Tiergarten",
+    "Treptow",
+    "Wedding",
+    "Weißensee",
+    "Zehlendorf"];
+
+    export const arrUhr = [
+        "alle Uhrzeiten",
+        "tagsüber : 7h - 22h",
+        "nachts : 22h - 7h",
+        ];
+
+//Liste fuer die Bezirke
+export const arrDeliktform = [
+    "Anfragen wegen Polizeimaßnahmen",
+    "Bedrohung durch Familie/ Gefahr von Zwangsverpartnerung",
+    "Beleidigung (auch Volksverhetzung)",
+    "Diebstahl in Wohnung",
+    "Diebstahl in Wohnung, sex Interesse vorgetäuscht",
+    "Diebstahl (Straße, Lokal) mit sex Nötigung",
+    "Diebstahl (Straße, Lokal), Trickdiebstahl",
+    "einfache Sachbeschädigung",
+    "Häusliche Gewalt (Körperverletzung, Nötigung)",
+    "Körperverletzung, gefährl Körperverletzung (auch Versuch)",
+    "Nötigung und Bedrohung",
+    "Raub auf Straße",
+    "Raub in Wohnung",
+    "sexuelle Nötigung, Vergewaltigung",
+    "Stalking",
+    "Tötung/ mit Todesfolge",
+    "anderes",
+];
+
+export const arrJahr = [ "alle", 2019, 2020 , 2021, 2022] ;
+   
+
+
+
+
+
