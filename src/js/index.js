c285de21eaa7032b30970711ba8a575625ccb4ca
TiboLC <46757616+TiboLC@users.noreply.github.com>
2022-06-01 14:44:19 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -16,9 +16,8 @@ new OlMap();
 //Radio button mit Bezirken : im Moment sind Bezirken durch random erstellt und daher falsch!
 var txtBezirk = ['<p>Bezirk</p>'];
 for (let i=0;i<arrBezirke.length;i++)  {
-    let txt1;
-    if (i==0){ txt1=  '<input type="radio" id="'+arrBezirke[i]+'" name="txtbezirk" value="'+i+'" checked="checked">';}
-    else {txt1 = '<input type="radio" id="'+arrBezirke[i]+'" name="txtbezirk" value="'+i+'">' ; }
+    var j=i+1;
+    let txt1 = '<input type="radio" id="'+arrBezirke[i]+'" name="txtbezirk" value="'+j+'">' ; 
     let txt2  = '<for"'+arrBezirke[i]+'">'+arrBezirke[i]+'</label><br>' ;
     txtBezirk += txt1+txt2;
     };    
@@ -27,9 +26,8 @@ $('#contentBezirk').html(txtBezirk);
 //Radio button mit Uhrzeiten
 var txtUhr = ['<p>Uhrzeiten</p>'];
 for (let i=0;i<arrUhr.length;i++)  {
-    let txt3;
-    if (i==0){ txt3=  '<input type="radio" id="'+arrUhr[i]+'" name="txtUhr" value="'+i+'" checked="checked">';}
-    else {txt3 = '<input type="radio" id="'+arrUhr[i]+'" name="txtUhr" value="'+i+'">' ;}
+    var j=i+1;
+    let txt3 = '<input type="radio" id="'+arrUhr[i]+'" name="txtUhr" value="'+j+'">' ;
     let txt4  = '<for"'+arrUhr[i]+'">'+arrUhr[i]+'</label><br>' ;
     txtUhr += txt3+txt4;
     };    
@@ -38,22 +36,18 @@ $('#divUhrzeit').html(txtUhr);
 //Radio button mit Jahren
 var txtJahr = ['<p>Jahr</p>'];
 for (let i=0;i<arrJahr.length;i++)  {
-    let txt5;
-    if (i == 0) {txt5 = '<input type="radio" id="'+arrJahr[i]+'" name="txtJahr" value="'+i+'" checked="checked">' ;}
-    else  {txt5 = '<input type="radio" id="'+arrJahr[i]+'" name="txtJahr" value="'+i+'">' ;}
+    var j=i+1;
+    let txt5 = '<input type="radio" id="'+arrJahr[i]+'" name="txtJahr" value="'+j+'">' ;
     let txt6  = '<for"'+arrJahr[i]+'">'+arrJahr[i]+'</label><br>' ;
     txtJahr += txt5+txt6;
     };    
 $('#divJahr').html(txtJahr);
 
-//Radio button mit Deliktformen
-var txtDelikt = ['<p>Deliktformen</p>'];
+//Drop down menu für Deliktformen
+var txtDelikt = ['<label for="deliktListe">Deliktformen: </label><select id="deliktListe" name="deliktListe">'];
 for (let i=0;i<arrDelikt.length;i++)  {
-    let txt7;
-    if (i == 0) {txt7 = '<input type="radio" id="'+arrDelikt[i]+'" name="txtDelikt" value="'+i+'" checked="checked">' ;}
-    else { txt7 = '<input type="radio" id="'+arrDelikt[i]+'" name="txtDelikt" value="'+i+'">' ;}
-    let txt8  = '<for"'+arrDelikt[i]+'">'+arrDelikt[i]+'</label><br>' ;
-    txtDelikt += txt7+txt8;
+    let txt7 = '<option value="'+i+'">'+arrDelikt[i]+'</option>' ; 
+    txtDelikt += txt7;
     };    
 $('#divDelikt').html(txtDelikt);
 
